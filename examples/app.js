2e317dd76de860c86f94d1ec4ee12564b8c2e9ea
helloneele <hello@neele.codes>
2018-10-27 10:29:52 -0400

    feat: add CSS support

--- examples/app.js
+++ examples/app.js
@@ -1,9 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import {css} from "emotion"
+
 import {Confetti} from "../src/index.js"
 
+const customStyles = css`
+  background: pink;
+`
+
 function App() {
-  return <Confetti>Hooray!</Confetti>
+  return <Confetti className={customStyles}>Hooray! 🎉</Confetti>
 }
 
 const app = document.querySelector("#app")
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from "react"
+import {css} from "emotion"
+import classNames from "classnames"
 
-export function Confetti({children}) {
-  return <div>{children}</div>
+const styles = css`
+  color: hotpink;
+`
+
+export function Confetti({children, className, ...props}) {
+  const mergedClass = classNames(styles, className)
+
+  return (
+    <div className={mergedClass} {...props}>
+      {children}
+    </div>
+  )
 }
